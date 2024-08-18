42534a26f3ebf7f978d8b91b90ba8870f126e049
Romanov Vitaly <9773411353@mail.ru>
2024-08-18 22:52:27 +0300

    d

--- client_avenu/src/app/[locale]/layout.tsx
+++ client_avenu/src/app/[locale]/layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import type { Metadata } from "next";
 import { NextIntlClientProvider } from 'next-intl';
 import { getMessages, unstable_setRequestLocale } from 'next-intl/server';
@@ -6,8 +6,18 @@ import Header from "@/shared/components/Header/Header";
 import Footer from "@/shared/components/Footer/Footer";
 import { MainProvider, useMainContext } from "@/widgets/Contex/MainProvider";
 import '../globals.css'
+import Script from "next/script";
+import YandexMetrika from "@/shared/components/YandexMetrika/YandexMetrika";
+import { GoogleAnalytics } from '@next/third-parties/google'
+import { Montserrat } from 'next/font/google'
 
 
+const montserrat = Montserrat({
+  weight: ['400', '500', '600', '700', '900'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 const locales = ['en', 'ru'];
 
@@ -35,8 +45,37 @@ export default async function RootLayout({
 
 
   return (
-    <html lang={locale}>
+    <html lang={locale} className={montserrat.className}>
+      <head>
+
+      </head>
       <body className="wrapper">
+        <Script id="metrika-counter" strategy="afterInteractive">
+          {
+            `(function (m, e, t, r, i, k, a) {
+              m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+              m[i].l = 1 * new Date();
+              for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } }
+              k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+            })
+              (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+        
+            ym(97878080, "init", {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true,
+              webvisor: true,
+              params: {
+                'sameSite': 'None',
+                'secure': true
+              }
+            })`
+          }
+        </Script>
+        <Suspense fallback={<></>}>
+          <YandexMetrika />
+        </Suspense>
+        <GoogleAnalytics gaId="G-074Y6C99Q5" />
         <NextIntlClientProvider messages={messages}>
           <MainProvider>
             <Header />
